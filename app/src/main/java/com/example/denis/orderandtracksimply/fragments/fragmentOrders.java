c4b73e758f9bfdacb40491d4d3282332da727228
krutk <artstyleflax@gmail.com>
2017-02-06 14:15:48 +0100

    бета версия работы get запроса [2]

--- app/src/main/java/com/example/denis/orderandtracksimply/fragments/fragmentOrders.java
+++ app/src/main/java/com/example/denis/orderandtracksimply/fragments/fragmentOrders.java
@@ -24,13 +24,17 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 public class fragmentOrders extends Fragment
 {
-    Button button;
-    TextView title;
+    //everything I need to change/use
+    Button button, but_R, but_L;
+    TextView order, client, status, count;
+    int c_page = 1;
 
     private static String TAG = fragmentOrders.class.getSimpleName();
+    ArrayList<String> data;
 
     // Progress dialog
     private ProgressDialog pDialog;
@@ -44,13 +48,20 @@ public class fragmentOrders extends Fragment
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.fragment_orders, null);
 
+        //catching button/views
         button = (Button) v.findViewById(R.id.button);
-        title = (TextView) v.findViewById(R.id.title);
+        but_R = (Button) v.findViewById(R.id.but_R);
+        but_L = (Button) v.findViewById(R.id.but_L);
+
+        order = (TextView) v.findViewById(R.id.order);
+        client = (TextView) v.findViewById(R.id.client);
+        status = (TextView) v.findViewById(R.id.status);
+
+        count = (TextView) v.findViewById(R.id.count);
 
         pDialog = new ProgressDialog(getActivity());
         pDialog.setMessage("Please wait...");
@@ -66,17 +77,19 @@ public class fragmentOrders extends Fragment
             }
         });
 
+        //setting pages scroll
+        but_L.setOnClickListener(Scrolling);
+        but_R.setOnClickListener(Scrolling);
+
         return v;
     }
 
-    /**
-     * Method to make json array request where response starts with [
-     */
+    //Method to make json array request where response starts with [
     private void makeJsonArrayRequest()
     {
         showpDialog();
-
-        String urlJsonArry = "https://api.myjson.com/bins/l9fsx";
+        data = new ArrayList<>();
+        String urlJsonArry = "http://10.0.3.2/HttpServ/hs/order-services/list";
         JsonArrayRequest req = new JsonArrayRequest(urlJsonArry,
                 new Response.Listener<JSONArray>()
                 {
@@ -92,20 +105,37 @@ public class fragmentOrders extends Fragment
                             jsonResponse = "";
                             for (int i = 0; i < response.length(); i++)
                             {
-
+                                //parsing data we receive
                                 JSONObject person = (JSONObject) response.get(i);
 
-                                String order = new String(person.getString("order").getBytes("ISO-8859-1"), "UTF-8");
+                                /*String order = new String(person.getString("order").getBytes("ISO-8859-1"), "UTF-8");
                                 String client = new String(person.getString("client").getBytes("ISO-8859-1"), "UTF-8");
-                                String status = new String(person.getString("status").getBytes("ISO-8859-1"), "UTF-8");
+                                String status = new String(person.getString("status").getBytes("ISO-8859-1"), "UTF-8");*/
 
-                                jsonResponse += "Order: " + order + "\n\n";
-                                jsonResponse += "Client: " + client + "\n\n";
-                                jsonResponse += "Status: " + status + "\n\n\n";
+                                String order = person.getString("order");
+                                String client = person.getString("client");
+                                String status = person.getString("status");
+
+                                jsonResponse = "";
+
+                                //packing into array list
+                                jsonResponse += order + "%";
+                                jsonResponse += client + "%";
+                                jsonResponse += status;
+
+                                data.add(jsonResponse);
                             }
 
-                            title.setText(jsonResponse);
+                            String one_note[] = data.get(0).split("%");  // splitting data
+                            order.setText(one_note[0]);
+                            client.setText("Клиент: " + one_note[1]);
+                            status.setText("Статус: " + one_note[2]);
 
+                            count.setText("1/" + data.size());
+                            c_page = 1;
+
+                            but_L.setEnabled(true);
+                            but_R.setEnabled(true);
                         }
                         catch (JSONException e)
                         {
@@ -114,10 +144,10 @@ public class fragmentOrders extends Fragment
                                     "Error: " + e.getMessage(),
                                     Toast.LENGTH_LONG).show();
                         }
-                        catch (UnsupportedEncodingException e)
+                        /*catch (UnsupportedEncodingException e)
                         {
                             e.printStackTrace();
-                        }
+                        }*/
 
                         hidepDialog();
                     }
@@ -136,6 +166,41 @@ public class fragmentOrders extends Fragment
         // Adding request to request queue
         AppController.getInstance().addToRequestQueue(req);
     }
+
+    void setActualData(int n)
+    {
+        String one_note[] = data.get(n - 1).split("%");  // splitting data
+        order.setText(one_note[0]);
+        client.setText("Клиент: " + one_note[1]);
+        status.setText("Статус: " + one_note[2]);
+
+        count.setText(n + "/" + data.size());
+        c_page = n;
+    }
+
+    View.OnClickListener Scrolling = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            switch (v.getId())
+            {
+                case R.id.but_L:
+                    if (c_page > 1)
+                    {
+                        setActualData(c_page - 1);
+                    }
+                    break;
+
+                case R.id.but_R:
+                    if (c_page < data.size())
+                    {
+                        setActualData(c_page + 1);
+                    }
+                    break;
+            }
+        }
+    };
 
     private void showpDialog()
     {
